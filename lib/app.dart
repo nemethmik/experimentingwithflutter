fc9ecf235f4b1cef0859d72cf3b23bced0e310ca
Miklos Nemeth <nemethmiklos1960@gmail.com>
2019-01-14 12:47:27 +0100

    App changed to a stateful widget

--- lib/app.dart
+++ lib/app.dart
@@ -1,15 +1,17 @@
 import "package:flutter/material.dart";
-class App extends StatelessWidget {
+class App extends StatefulWidget{createState() => AppStateKeeper();}
+//class App extends StatelessWidget {
+class AppStateKeeper extends State<StatefulWidget> {
   final barColor = const Color(0xFF26ae60);
   final bgColor = const Color(0xFFDAE0E2);
+  int myValue = 1;
   Widget build(context) {
     return MaterialApp(
       home: Scaffold(
+        body: Text("Clicked $myValue times"),
         floatingActionButton: FloatingActionButton(
           backgroundColor: barColor,
-          onPressed: (){
-
-          },
+          onPressed: (){this.setState((){myValue += 1;});},
           child: Icon(Icons.add),        
         ),
         backgroundColor: bgColor,
