a82c10f9980ba74d3b579ca08b77f1d8c8be1311
Miklos Nemeth <nemethmiklos1960@gmail.com>
2019-08-15 16:55:43 +0200

    _makeListViewFilledStackWay added

--- provider_arc/lib/posts_listview.dart
+++ provider_arc/lib/posts_listview.dart
@@ -11,25 +11,28 @@ class PostsListView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BaseWidget<PostsModel>(
-        model: PostsModel(api: Provider.of(context)),
-        onModelReady: (model) => model.getPosts(Provider.of<User>(context).id),
-        builder: (context, model, child) => model.busy
-            ? Center(
-                child: CircularProgressIndicator(),
-              )
-            : ListView.builder(
-                itemCount: model.posts.length,
-                itemBuilder: (context, index) => PostListItem(
-                      post: model.posts[index],
-                      onTap: () {
-                        Navigator.pushNamed(
-                          context,
-                          RoutePaths.Post,
-                          arguments: model.posts[index],
-                        );
-                      },
-                    ),
-              ));
+    return _makeListViewFilledStackWay(context);
   }
+  _makeListViewFilledStackWay(BuildContext context) =>
+    BaseWidget<PostsModel>(
+      model: PostsModel(api: Provider.of(context)),
+      onModelReady: (model) => model.getPosts(Provider.of<User>(context).id),
+      builder: (context, model, child) => model.busy
+        ? Center(
+            child: CircularProgressIndicator(),
+          )
+        : ListView.builder(
+            itemCount: model.posts.length,
+            itemBuilder: (context, index) => PostListItem(
+              post: model.posts[index],
+              onTap: () {
+                Navigator.pushNamed(
+                  context,
+                  RoutePaths.Post,
+                  arguments: model.posts[index],
+                );
+              },
+            ),
+          ));
+
 }
